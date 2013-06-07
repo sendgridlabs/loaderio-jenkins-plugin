80c2ed96f6a1e8e14fad1f7f2309c8319a9f22e6
Sergii Boiko <cris.kiev@gmail.com>
2013-06-07 18:01:08 +0300

    Add partial LoaderAPI implementation

--- src/main/java/io/loader/jenkins/api/LoaderAPI.java
+++ src/main/java/io/loader/jenkins/api/LoaderAPI.java
@@ -2,34 +2,72 @@ package io.loader.jenkins.api;
 
 import java.io.IOException;
 import java.io.PrintStream;
+import java.net.URI;
 
 import javax.servlet.ServletException;
 
 import net.sf.json.JSONException;
+import net.sf.json.JSONSerializer;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONArray;
+import net.sf.json.JSON;
 
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.util.EntityUtils;
 
 public class LoaderAPI {
-	
-	PrintStream logger = new PrintStream(System.out);
-	DefaultHttpClient httpClient;
-	String apiKey;
+    static final String baseApiUri = "http://api.loader.io/v2/";
 
-	public LoaderAPI(String apiKey) {
-		this.apiKey = apiKey;
-        try {
-            httpClient = new DefaultHttpClient();
-        } catch (Exception ex) {
-            logger.format("error Instantiating HTTPClient. Exception received: %s", ex);
-        }
+    PrintStream logger = new PrintStream(System.out);
+    String apiKey;
+
+    public LoaderAPI(String apiKey) {
+        logger.println("in #LoaderAPI, apiKey: " + apiKey);
+        this.apiKey = apiKey;
     }
-	
-	public Boolean getTestApi() throws JSONException, IOException, ServletException {
-		if (apiKey == null || apiKey.trim().isEmpty()) {
+
+    public JSON getTests() {
+        logger.println("in #getTests");
+        String result = doRequest(new HttpGet(), "tests");
+        JSON list = JSONSerializer.toJSON(result);
+        logger.println("Result :::\n" + list.toString());
+        return list;
+    }
+
+    public Boolean getTestApi() {
+        if (apiKey == null || apiKey.trim().isEmpty()) {
             logger.println("getTestApi apiKey is empty");
             return false;
         }
-		return true;
+        JSON tests = getTests();
+        if (null == tests) {
+            logger.println("invalid ApiKey");
+        }
+        return true;
     }
 
+    private String doRequest(HttpRequestBase request, String path) {
+        stuffHttpRequest(request, path);
+        DefaultHttpClient client = new DefaultHttpClient();
+        try {
+            return EntityUtils.toString(client.execute(request).getEntity());
+        } catch (IOException ex) {
+            logger.format("error Instantiating HTTPClient. Exception received: %s", ex);
+            throw new RuntimeException("Error connection to remote API");
+        }
+    }
+
+    private void stuffHttpRequest(HttpRequestBase request, String path) {
+        URI fullUri = null;
+        try {
+            fullUri = new URI(baseApiUri + path);
+        } catch (java.net.URISyntaxException ex) {
+            logger.format("Exception received: %s", ex);
+        }
+        request.setURI(fullUri);
+        request.addHeader("Content-Type", "application/json");
+        request.addHeader("loaderio-Auth", apiKey);
+    }
 }
